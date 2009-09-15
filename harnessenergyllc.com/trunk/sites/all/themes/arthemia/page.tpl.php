c2b8156cd566cdced977f4c8c43aa9c0dc8ffb9e
warner.jack <warner.jack@9df07662-117f-11de-a46f-cd5bcf99231e>
2009-09-15 13:12:47 +0000

    sept 15 email from Ben

--- harnessenergyllc.com/trunk/sites/all/themes/arthemia/page.tpl.php
+++ harnessenergyllc.com/trunk/sites/all/themes/arthemia/page.tpl.php
@@ -4,7 +4,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
   <head>
-    <title><?php print $head_title ?></title>
+    <title>Harness Energy, LLC | Wind Resource Assessment</title>
     <?php print $head ?>
     <?php print $styles ?>
     <?php print $scripts ?>
